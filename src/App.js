42812ebb4d73995e3abd45f702a0349ceb9da19c
Joao Otavio Manieri <jotamanieri@ufu.br>
2023-01-26 16:02:01 -0300

    add button inside divs of card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import GifList from './components/GiftList';
+import Container from './components/Like';
+import Like from './Context/LikeContext';
 
 function App() {
   
   
   return (
     <div>
+<Like>
   <GifList/>
+  </Like>
     </div>
   );
 }
--- src/components/GiftList/index.js
+++ src/components/GiftList/index.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import './style.css'
+import { useTheme, themes } from "../../Context/LikeContext";
 
 
-function GifList() {
+function GifList({ children }) {
   const [gifs, setGifs] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const { theme, setTheme } = useTheme();
 
   function fetchGif() {
     fetch(`https://api.giphy.com/v1/gifs/search?api_key=0KsgY9RDhjhS6Nrvxaojmz7FYvEi21fQ&q=${searchTerm}&limit=20`)
@@ -13,29 +15,35 @@ function GifList() {
       .catch(error => console.log(error));
   }
 
+
   return (
     <div>
       <h2>Gif De Malandro</h2>
       <input type="text" value={searchTerm} onBlur={fetchGif} onChange={e => setSearchTerm(e.target.value)} placeholder="Procura o gif q vc quer aqui malandro" />
+      <div className='cards'>
         {gifs.map(gif => (
-          <div key={gif.id} className='cards'>
-              <div className='card'>
-                <img src={gif.images.original.url} alt={gif.title} />
-              </div>
-              <div className='card'>
-                <img src={gif.images.original.url} alt={gif.title} />
-              </div>
-              <div className='card'>
-                <img src={gif.images.original.url} alt={gif.title} />
-              </div>
-              <div className='card'>
-                <img src={gif.images.original.url} alt={gif.title} />
-              </div>
-              <div className='card'>
-                <img src={gif.images.original.url} alt={gif.title} />
-              </div>
+
+          <div key={gif.id} className='card'>
+            <img src={gif.images.original.url} alt={gif.title} />
+
+            <div
+      style={{
+        background: theme.colors.background,
+        color: theme.colors.text
+      }}
+    >
+      {children}
+      <hr />
+      <button
+        onClick={() =>
+          setTheme(theme === themes.light ? themes.dark : themes.light)
+        }
+      >
+        {theme === themes.light ? "Toggle dark mode" : "Toggle light mode"}
+      </button>
+    </div>
           </div>
-        ))}
+        ))}  </div>
     </div>
   );
 }
